1b6ddb1c71cf635c285c1c79bca005b057af78f0
Antepante123 <gusharanb@student.gu.se>
2022-10-24 18:32:44 +0200

    fixat todoapp

---
+++ todokort-master/lib/main.dart
@@ -0,0 +1,34 @@
+import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
+import 'package:http/http.dart' as http;
+import 'dart:convert';
+
+import 'CardListView.dart';
+import 'model.dart';
+
+void main() {
+  var state = MyState();
+  state.getCard();
+  runApp(
+    ChangeNotifierProvider(
+      create: (context) => state,
+      child: MyApp(),
+    ),
+  );
+}
+
+class MyApp extends StatelessWidget {
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      title: "Todo",
+      theme: ThemeData(
+        primarySwatch: Colors.yellow,
+        visualDensity: VisualDensity.adaptivePlatformDensity,
+      ),
+      home: CardsListView(),
+    );
+  }
+}
